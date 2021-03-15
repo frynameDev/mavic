fc30ec33b99aaa019604b83b138a063da5584b62
frynameDev <psimen1986@gmail.com>
2021-03-16 00:31:47 +0300

    added specification and questions css

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,10 @@ $(function () {
    });
 
 
+   // аккардион
+   $('.questions__item-title').on('click', function () {
+      $('.questions__item').removeClass('questions__item--active');
+      $(this).parent().addClass('questions__item--active');
+   });
+   //
 });
